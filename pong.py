a5f8fecac66bed685e3f2bfa089ec9738269863b
Pratik Mullick <pratik.mullick@gmail.com>
2019-06-03 01:03:59 +0530

    Reverting Back to Original Pong.py

--- pong.py
+++ pong.py
@@ -27,8 +27,37 @@ Object Oriented Pong is built with Python 3 and pygame, and is tested with
 Python3.6. and pygame 1.9.4."""
 
 import pygame
-import settings
 import random,time,sys
+
+class Settings():
+    """Stores All Settings"""
+
+    def __init__(self):
+        # Screen Settings
+        self.x_res = 640
+        self.y_res = 480
+        self.scr_bg = (0,0,0)
+        self.white = (255,255,255)
+        self.fps = 180
+
+        # Line Settings
+        self.line = 10
+        self.ctr_segment = 12
+
+        # Key Settings
+        self.exit_key = pygame.K_ESCAPE
+
+        # Ball Settings
+        self.b_size = int(self.line*2)
+        self.b_speed = 2
+
+        # Paddle Settings
+        self.p_width = self.line * 2
+        self.p_length = int(self.y_res / 12)
+
+        # AI settings
+        self.wait = 1
+        self.difficulty = self.b_speed / 2
 
 class GameFunctions():
     """A Class to store game functions"""
