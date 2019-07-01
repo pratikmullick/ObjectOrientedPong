19d1447f462823d9eda23baca9660a40cf306776
Pratik Mullick <pratikmullick@github.com>
2019-07-01 21:41:45 +0530

    Finished OpeningScreen class

--- screen.py
+++ screen.py
@@ -1,5 +1,7 @@
-import pygame, sys
+import pygame
+import pygame.freetype
 from options import Configuration       # For Debug Purposes Only
+import sys                              # For Debug Purposes Only
 
 class Display:
     """
@@ -14,63 +16,112 @@ class Display:
         self.surface.fill((0,0,0))
         pygame.display.update()
 
+
+
 class OpeningScreen(Display):
     """
-    Generates opening screen.
+    Generates opening screen to the Display (parent class).
     """
 
     def __init__(self, settings):
         super().__init__(settings)
-        self.logo_height = (self.settings.height * 2) // 5
-        self.logo_width = self.settings.width // 20
-        self.color = (192, 192, 192)
-        self.line = self.settings.width // 50
+        pygame.freetype.init()
 
-        # For code shortening (explicitly visual)
-        w = self.settings.width
+        # Shortening for easier typing
+        self.w = self.settings.width
+        self.h = self.settings.height
 
-        self.logo_x_pos = [x for x in range(w // 6, w - (w // 10), w // 5)]
-        self.logo_y_init = self.settings.height + (self.settings.height // 10)
-        self.logo_y_final = self.settings.height // 10
+        # Font Settings
+        self.font = pygame.freetype.Font("assets/saxmono.ttf", size = self.h // 25)
+        self.font_color = (255, 255, 255)
+        self.message = ["ONE PLAYER", "TWO PLAYER"]   
 
-        self.boards = [pygame.Rect(x, self.logo_y_final, self.logo_width, self.logo_height) for x in self.logo_x_pos]
-
-        self.logo = [
-                        [
-                            self.boards[0].bottomleft, self.boards[0].topleft, self.boards[0].topright,
-                            self.boards[0].midright, self.boards[0].midleft
-                        ],
-                        [
-                            self.boards[1].topleft, self.boards[1].topright, self.boards[1].bottomright,
-                            self.boards[1].bottomleft, self.boards[1].topleft
-                        ],
-                        [
-                            self.boards[2].bottomleft, self.boards[2].topleft, self.boards[2].bottomright,
-                            self.boards[2].topright
-                        ],
-                        [
-                            self.boards[3].topright, self.boards[3].topleft, self.boards[3].bottomleft,
-                            self.boards[3].bottomright,self.boards[3].midright, self.boards[3].center
-                        ]
-                    ]
+        # Automatically run function when class is initialized
+        self.fps_clock = pygame.time.Clock()
+        self.render_logo()
+        self.player_selection()
+        self.cursor()
+        pygame.display.update()
+        self.fps_clock.tick(self.settings.fps)
 
     def render_logo(self):
-        fps_clock = pygame.time.Clock()
-        for logo_chr in range(4):
-            pygame.draw.lines(self.surface, self.color, False, self.logo[logo_chr], self.line)
-        pygame.display.update()
-        fps_clock.tick(self.settings.fps)
+        """
+        Renders vectorized logo.
+        """
+        logo_height = (self.h * 2) // 5
+        logo_width = self.w // 20
+        logo_color = (255, 255, 255)
+        logo_line = self.w // 50
+        
+        logo_x_pos = [x for x in range(self.w // 6, self.w - (self.w // 10), self.w // 5)]
+        logo_y_init = self.h + (self.h // 10)
+        logo_y_final = self.h // 10
 
+        # Array of Rects to hold logo characters in place
+        boards = [pygame.Rect(x, logo_y_final, logo_width, logo_height) for x in logo_x_pos]
+
+        # Array of characters, with each character being an array of lines to render the logo
+        logo = [    
+                [boards[0].bottomleft, boards[0].topleft, boards[0].topright, boards[0].midright, boards[0].midleft],
+                [boards[1].topleft, boards[1].topright, boards[1].bottomright, boards[1].bottomleft,
+                 boards[1].topleft],
+                [boards[2].bottomleft, boards[2].topleft, boards[2].bottomright, boards[2].topright],
+                [boards[3].topright, boards[3].topleft, boards[3].bottomleft, boards[3].bottomright,
+                 boards[3].midright, boards[3].center]
+               ]
+
+
+        for logo_chr in range(4):
+            pygame.draw.lines(self.surface, logo_color, False, logo[logo_chr], logo_line)
+
+    def player_selection(self):
+        """
+        Renders player selection menu.
+        """
+        self.gap = self.h // 10
+        self.height = 7 * self.gap
+
+        for player in self.message:
+            self.text_width = self.font.get_rect(player).width
+            self.starting = (self.w // 2) - (self.text_width // 2)
+            self.text_height = self.font.get_rect(player).height
+            self.font.render_to(self.surface, (self.starting, self.height), player, fgcolor=self.font_color)
+            self.height += self.gap
+
+    def cursor(self):
+
+        board = pygame.Rect(self.starting - (self.w // 10), self.gap * 7, self.text_height * 2, self.text_height)
+        cursor = [board.midtop, board.midright, board.midleft, board.midright, board.midbottom]
+        pygame.draw.lines(self.surface, self.font_color, False, cursor, self.w // 250)
+
+
+class GameScreen(Display):
+    """
+    Renders the game screen to the Display (parent) class.
+    """
+
+    def __init__(self, settings):
+        super().__init__(settings)
 
 # Debug
 if __name__ == "__main__":
-    confile = ".pong.conf"
-    ops = OpeningScreen(Configuration(confile))
+    confy = Configuration(".pong.conf")
+    Display(confy)
+    # OpeningScreen(confy)
 
     while True:
-        ops.render_logo()
         for event in pygame.event.get():
+            pygame.event.pump()
+            key = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif key[pygame.K_ESCAPE]:
+                pygame.quit()
+                sys.exit()
+            elif key[pygame.K_RETURN]:
+                active = True
+
+    if active:
+        print("Changing to Game Screen")
 
